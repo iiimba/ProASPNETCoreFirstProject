5dfa85f39dbcbd0aa15f8516f72e0b387e83153d
Vlad Misnichenko <vmo@vava.cars>
2021-05-04 21:52:00 +0300

    Add project files.

---
+++ ProASPNETCoreFirstProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProASPNETCoreFirstProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
