54d90b4785c18b035fa4f2140c8424588295b209
Vlad Misnichenko <vmo@vava.cars>
2021-05-24 11:15:34 +0300

    Added configuration examples

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -4,117 +4,60 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Platform.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace Platform
 {
     public class Startup
     {
-        public Startup(IConfiguration config)
+        public Startup(IConfiguration configService)
         {
-            Configuration = config;
+            Configuration = configService;
         }
+        private IConfiguration Configuration { get; set; }
 
-        private IConfiguration Configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<MessageOptions>(options =>
-            {
-                options.CityName = "Albany";
-            });
-
-            services.Configure<RouteOptions>(opts =>
-            {
-                opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
-            });
-
-            ///services.AddScoped(serviceProvider => {
-            ///    var typeName = Configuration["services:IResponseFormatter"];
-            ///    return (IResponseFormatter)ActivatorUtilities.CreateInstance(
-            ///        serviceProvider, 
-            ///        typeName == null
-            ///            ? typeof(GuidService) 
-            ///            : Type.GetType(typeName, true));
-            ///            
-            ///});
-            ///
-            services.AddScoped<IResponseFormatter, TextResponseFormatter>();
-            services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
-            services.AddScoped<IResponseFormatter, GuidService>();
-
-            services.AddSingleton(typeof(ICollection<>), typeof(List<>));
+            services.Configure<MessageOptions>(Configuration.GetSection("Location"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
+                RequestPath = "/files"
+            });
+
             app.UseRouting();
 
-            //app.UseMiddleware<WeatherMiddleware>();
+            app.UseMiddleware<LocationMiddleware>();
 
-            //app.Use(async (context, next) =>
-            //{
-            //    if (context.Request.Path == "/middleware/function")
-            //    {
-            //        var formatter = context.RequestServices.GetService<IResponseFormatter>();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    logger.LogTrace("Response for / started");
+                    logger.LogDebug("Response for / started");
+                    logger.LogInformation("Response for / started");
+                    logger.LogError("Response for / started");
+                    logger.LogCritical("Response for / started");
 
-            //        //var service = app.ApplicationServices.GetRequiredService<IResponseFormatter>();
+                    await context.Response.WriteAsync("Hello World!");
 
-            //        await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
-            //    }
-            //    else
-            //    {
-            //        await next();
-            //    }
-            //});
-
-            app.UseEndpoints(endpoints => {
-                //endpoints.MapEndpoint<WeatherEndpoint>("/endpoint/class");
-                //endpoints.MapGet("/endpoint/function", async context =>
-                //{
-                //    var formatter = context.RequestServices.GetService<IResponseFormatter>();
-
-                //    await formatter.Format(context, "Endpoint Function: It is sunny in LA");
-                //});
-
-                //endpoints.MapGet("/single", async context => {
-                //    var formatter = context.RequestServices.GetService<IResponseFormatter>();
-                //    await formatter.Format(context, "Single service");
-                //});
-
-                //endpoints.MapGet("/", async context => {
-                //    var formatter = context.RequestServices.GetServices<IResponseFormatter>().First(f => f.RichOutput);
-                //    await formatter.Format(context, "Multiple services");
-                //});
-
-                endpoints.MapGet("/string", async context => {
-                    var collection = context.RequestServices.GetService<ICollection<string>>();
-                    collection.Add($"Request: {DateTime.Now.ToLongTimeString()}");
-                    foreach (var str in collection)
-                    {
-                        await context.Response.WriteAsync($"String: {str}\n");
-                    }
-
-                });
-                endpoints.MapGet("/int", async context => {
-                    var collection = context.RequestServices.GetService<ICollection<int>>();
-                    collection.Add(collection.Count() + 1);
-                    foreach (var val in collection)
-                    {
-                        await context.Response.WriteAsync($"Int: {val}\n");
-                    }
+                    logger.LogDebug("Response for / completed");
                 });
             });
         }
