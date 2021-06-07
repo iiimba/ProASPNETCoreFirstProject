de8d8e5b0e5430da1cb7351d13a1e7dfc31a1401
Vlad Misnichenko <vmo@vava.cars>
2021-06-07 11:38:24 +0300

    Added another examples with razor views

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using WebApp.Models;
 
@@ -15,25 +16,19 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
-            var prod = await context.Products.FindAsync(id);
-            if (prod.CategoryId == 1)
-            {
-                return View("Watersports", prod);
-            }
-            else
-            {
-                return View(prod);
-            }
-        }
+            ViewBag.AveragePrice = await context.Products.AverageAsync(p => p.Price);
 
-        public IActionResult Common()
-        {
-            return View();
+            return View(await context.Products.FindAsync(id));
         }
 
         public IActionResult List()
         {
             return View(context.Products);
+        }
+
+        public IActionResult Html()
+        {
+            return View((object)"This is a <h3><i>string</i></h3>");
         }
     }
 }
