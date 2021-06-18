8a9b42c1df1a1b059aadec8c6829a5b6d457bb25
Vlad Misnichenko <vmo@vava.cars>
2021-06-18 13:47:01 +0300

    Added examples with form tag helpers

--- WebApp/Models/Product.cs
+++ WebApp/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
 {
@@ -9,6 +10,7 @@ namespace WebApp.Models
         public string Name { get; set; }
 
         [Column(TypeName = "decimal(8, 2)")]
+        [DisplayFormat(DataFormatString = "{0:c2}", ApplyFormatInEditMode = true)]
         public decimal Price { get; set; }
 
         public long CategoryId { get; set; }
