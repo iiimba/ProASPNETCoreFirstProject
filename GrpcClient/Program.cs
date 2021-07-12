b5fdb0b31686b359c5849cfab05f76c02452f847
Vlad Misnichenko <vmo@vava.cars>
2021-07-12 22:54:14 +0300

    Added example with gRPC

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var request = new HelloRequest { Name = "Vlad" };
+            var response = await client.SayHelloAsync(request);
+
+            Console.WriteLine(response);
+        }
+    }
+}
