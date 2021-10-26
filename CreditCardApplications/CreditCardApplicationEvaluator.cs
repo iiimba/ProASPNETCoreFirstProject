652223aeadb9cf1719ce0da1b84ab4590c8c49c4
Vlad Misnichenko <vmo@vava.cars>
2021-10-26 05:50:28 +0300

    Added project for Moq tests

---
+++ CreditCardApplications/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardApplications
+{
+    public class CreditCardApplicationEvaluator
+    {
+        private const int AutoReferralMaxAge = 20;
+        private const int HighIncomeThreshold = 100_000;
+        private const int LowIncomeThreshold = 20_000;
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (application.GrossAnnualIncome >= HighIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoAccepted;
+            }
+
+            if (application.Age <= AutoReferralMaxAge)
+            {
+                return CreditCardApplicationDecision.ReferrerToHuman;
+            }
+
+            if (application.GrossAnnualIncome <= LowIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoDeclined;
+            }
+
+            return CreditCardApplicationDecision.ReferrerToHuman;
+        }
+    }
+}
