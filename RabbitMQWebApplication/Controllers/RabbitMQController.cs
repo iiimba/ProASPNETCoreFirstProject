3e111284c041d96513681cae3cfba301dcdf1bc8
Vlad Misnichenko <vmo@vava.cars>
2021-12-18 08:50:37 +0200

    RabbitMQ project init commit

---
+++ RabbitMQWebApplication/Controllers/RabbitMQController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RabbitMQController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
